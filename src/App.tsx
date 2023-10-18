626ee3b5a68b25b059cf0e3abe4b73f7955692a4
MuhammadAslam-K <aslamka.2k3@gmail.com>
2023-10-18 23:02:18 +0530

    initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
+
+    </>
+  )
+}
+
+export default App
